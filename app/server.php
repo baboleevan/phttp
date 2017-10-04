b51b9faea2de64af6f7f49676d55f4fd610a0f5b
youngiggy <youngiggy@gmail.com>
2017-10-05 01:51:42 +0900

    the simplest server

---
+++ app/server.php
@@ -0,0 +1,15 @@
+<?php
+$server = stream_socket_server("tcp://127.0.0.1:1337", $errno, $errorMessage);
+
+if ($server === false) {
+    throw new UnexpectedValueException("Could not bind to socket: $errorMessage");
+}
+
+while (true) {
+    $client = @stream_socket_accept($server);
+
+    if ($client) {
+        stream_copy_to_stream($client, $client);
+        fclose($client);
+    }
+}
