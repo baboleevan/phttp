7e8c459df81fb3d036df2bcea1b8de0ef77e19fb
youngiggy <youngiggy@gmail.com>
2017-10-16 21:34:33 +0900

    add simple client for test

---
+++ app/client.php
@@ -0,0 +1,41 @@
+<?php
+$client = stream_socket_client("tcp://localhost:1337", $errno, $errorMessage);
+if ($client === false) {
+    throw new UnexpectedValueException("Failed to connect: $errno - $errorMessage");
+}
+
+/*
+ * generate a message
+ */
+$startLine = 'GET / HTTP/1.0';
+
+$headers = [
+    'Host: localhost',
+    'Accept: */*',
+];
+$header = implode(PHP_EOL, $headers);
+
+$emptyLine = PHP_EOL;
+
+$body = '';
+
+/*
+ * request
+ */
+$message = implode(PHP_EOL, [
+    $startLine,
+    $header,
+    $emptyLine,
+    $body,
+]);
+fwrite($client, $message);
+
+/*
+ * response
+ */
+echo stream_get_contents($client);
+
+/*
+ * bye bye
+ */
+fclose($client);
