0f40106af004ed61a51e4f8a9039aea3a4242ebd
youngiggy <youngiggy@gmail.com>
2017-10-23 23:09:19 +0900

    첫 HTML 파일 서빙

--- app/server.php
+++ app/server.php
@@ -9,12 +9,22 @@ while (true) {
     $client = @stream_socket_accept($server);
 
     if ($client) {
+        //todo 요청(request) 처리 모듈로 분리하기
         $request= fread($client, 1024);
+
+        //todo 응답(response) 처리 모듈로 분리하기
+        $filename = '../public/index.html';
+        if (!($fp = fopen($filename, 'r'))) {
+            $response = 'HTTP/1.0 404 파일 없음' . PHP_EOL;
+            fwrite($client, $response, strlen($response));
+            fclose($client);
+        }
+        $responseBody = fread($fp, filesize($filename));
 
         $response = 'HTTP/1.0 200 OK' . PHP_EOL;
         $response .= 'Content-Type: text/html' . PHP_EOL;
         $response .= PHP_EOL;
-        $response .= 'you sent :' . PHP_EOL . $request . PHP_EOL;
+        $response .= $responseBody . PHP_EOL;
 
         fwrite($client, $response, strlen($response));
         fclose($client);
