7bbb0387b843b2fd21100653967b3706d5d8ab97
youngiggy <youngiggy@gmail.com>
2017-10-23 21:34:06 +0900

    첫 HTTP 서버 - 소스 정리

--- app/server.php
+++ app/server.php
@@ -9,14 +9,14 @@ while (true) {
     $client = @stream_socket_accept($server);
 
     if ($client) {
-        $clientSentData= fread($client, 1024);
+        $request= fread($client, 1024);
 
-        $responseBody = 'HTTP/1.0 200 OK' . PHP_EOL;
-        $responseBody .= 'Content-Type: text/html' . PHP_EOL;
-        $responseBody .= PHP_EOL;
-        $responseBody .= 'you sent :' . PHP_EOL . $clientSentData . PHP_EOL;
+        $response = 'HTTP/1.0 200 OK' . PHP_EOL;
+        $response .= 'Content-Type: text/html' . PHP_EOL;
+        $response .= PHP_EOL;
+        $response .= 'you sent :' . PHP_EOL . $request . PHP_EOL;
 
-        fwrite($client, $responseBody, strlen($responseBody));
+        fwrite($client, $response, strlen($response));
         fclose($client);
     }
 }
