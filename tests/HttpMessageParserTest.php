951e1e2fe97ed3a4ae1079f3a6451ac3f44df66b
youngiggy <youngiggy@gmail.com>
2017-11-03 00:09:29 +0900

    기본 키가 존재하는 지 assert 추가

--- tests/HttpMessageParserTest.php
+++ tests/HttpMessageParserTest.php
@@ -16,5 +16,12 @@ class HttpMessageParserTest extends TestCase
         $parser = new HttpMessageParser('');
         $result = $parser->getResult();
         $this->assertEquals(true, is_array($result));
+
+        $this->assertArrayHasKey('start_line', $result, 'key start_line exists');
+        $this->assertArrayHasKey('method', $result, 'key method exists');
+        $this->assertArrayHasKey('uri', $result, 'key uri exists');
+        $this->assertArrayHasKey('version', $result, 'key version exists');
+        $this->assertArrayHasKey('headers', $result, 'key headers exists');
+        $this->assertArrayHasKey('body', $result, 'key body exists');
     }
 }
