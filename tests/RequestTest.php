04980c5ce52a069e70d60ef34c1fb916d1a65f87
youngiggy <youngiggy@gmail.com>
2017-11-02 21:45:47 +0900

    getResourcePath 메소드 테스트

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -4,5 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
-
+    public function testGetResourcePath(): void
+    {
+        $request = new Request([]);
+        $this->assertEquals('/index.html', $request->getResourcePath());
+    }
 }
