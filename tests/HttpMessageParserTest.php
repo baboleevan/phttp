0e9d2deb6b5172f8c9162d235b852d30a2567a8a
youngiggy <youngiggy@gmail.com>
2017-11-08 20:15:21 +0900

    테스트 코드를 간결하게

--- tests/HttpMessageParserTest.php
+++ tests/HttpMessageParserTest.php
@@ -11,11 +11,16 @@ class HttpMessageParserTest extends TestCase
         $result = $parser->getResult();
         $this->assertEquals(true, is_array($result));
 
-        $this->assertArrayHasKey('start_line', $result, 'key "start_line" not exists');
-        $this->assertArrayHasKey('method', $result, 'key "method" not exists');
-        $this->assertArrayHasKey('uri', $result, 'key "uri" not exists');
-        $this->assertArrayHasKey('version', $result, 'key "version" not exists');
-        $this->assertArrayHasKey('headers', $result, 'key "headers" not exists');
-        $this->assertArrayHasKey('body', $result, 'key "body" not exists');
+        $requiredFields = [
+            'start_line',
+            'method',
+            'uri',
+            'version',
+            'headers',
+            'body',
+        ];
+        foreach ($requiredFields as $field) {
+            $this->assertArrayHasKey($field, $result, 'key "' . $field . '" does not exist');
+        }
     }
 }
