16df38de752c37436bf004fbf158c9bf4a412ce0
youngiggy <youngiggy@gmail.com>
2017-11-08 23:26:03 +0900

    CRLF로 나누고 시작줄을 공백으로 나누어 3개의 요소가 있는지 확인하는 큰 걸음

--- libs/HttpMessageParser.php
+++ libs/HttpMessageParser.php
@@ -20,7 +20,23 @@ class HttpMessageParser
 
     private function parse()
     {
-        if ($this->message == '') {
+        //메시지를 \r\n 혹은 \n으로 나누기
+        $messageSplit = preg_split('/(\r\n|\n)/', $this->message);
+        //각 줄의 양 끝 공백은 제거한다
+        array_walk($messageSplit, function(&$item){
+            $item = trim($item);
+        });
+
+        //시작줄은 있어야 한다
+        if (empty($messageSplit) || empty($messageSplit[0])) {
+            throw new Exception('Invalid message format.');
+        }
+
+        //시작줄을 whitespace로 나누기
+        $startLine = $messageSplit[0];
+        $startLineSplit = preg_split('/\s/', $startLine);
+
+        if (empty($startLineSplit) || count($startLineSplit) !== 3) {
             throw new Exception('Invalid message format.');
         }
     }
