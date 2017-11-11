a52d541f3de23e32ed8f295f04eb61282c1c6678
youngiggy <youngiggy@gmail.com>
2017-11-11 11:03:55 +0900

    시작줄을 세가지 요소로 분리하는 기능을 분리

--- libs/HttpMessageParser.php
+++ libs/HttpMessageParser.php
@@ -51,12 +51,31 @@ class HttpMessageParser
 
     private function verifyStartLine(): void
     {
+        $this->splitStartLineInto3Components();
+
         // TODO refactoring
-        //      시작줄을 공백으로 나눈다
         //      메소드를 확인한다
         //      URI를 확인한다
         //      버전을 확인한다
 
+        //check Method
+        if (empty($this->result['method']) || !in_array($this->result['method'], $this->supportMethods)) {
+            $this->throwInvalidMessageFormat();
+        }
+
+        //check URI
+        if (empty($this->result['uri']) || strpos($this->result['uri'], '/') !== 0) {
+            $this->throwInvalidMessageFormat();
+        }
+
+        //check version
+        if (empty($this->result['version']) || !preg_match('/HTTP\/\d+.\d+/', $this->result['version'])) {
+            $this->throwInvalidMessageFormat();
+        }
+    }
+
+    private function splitStartLineInto3Components()
+    {
         //시작줄은 있어야 한다
         if (empty($this->messageLines) || empty($this->messageLines[0])) {
             $this->throwInvalidMessageFormat();
@@ -70,23 +89,8 @@ class HttpMessageParser
             $this->throwInvalidMessageFormat();
         }
 
-        $method = $startLineSplit[0];
-        $uri = $startLineSplit[1];
-        $version = $startLineSplit[2];
-
-        //check Method
-        if (empty($method) || !in_array($method, $this->supportMethods)) {
-            $this->throwInvalidMessageFormat();
-        }
-
-        //check URI
-        if (empty($uri) || strpos($uri, '/') !== 0) {
-            $this->throwInvalidMessageFormat();
-        }
-
-        //check version
-        if (empty($version) || !preg_match('/HTTP\/\d+.\d+/', $version)) {
-            $this->throwInvalidMessageFormat();
-        }
+        $this->result['method'] = $startLineSplit[0];
+        $this->result['uri'] = $startLineSplit[1];
+        $this->result['version'] = $startLineSplit[2];
     }
 }
